277843f6efc2b2164bb4db6a14fddbb445ba793a
fxyyf98 <90344700+fxyyf98@users.noreply.github.com>
2021-10-11 00:53:53 -0400

    Create main.py

---
+++ project2/main.py
@@ -0,0 +1,11 @@
+import google_api
+import twitter_api
+
+trends = get_loc_trends("Boston")
+for trend in trends:
+    tweets = search_tweets(trend,10)
+    score = 0
+    for tweet in tweets:
+        score += analyze_text_sentiment(tweet)
+    print("trend:",trend)
+    print("score:",score)
